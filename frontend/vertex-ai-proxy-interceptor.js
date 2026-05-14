7b72aa0b7a7b6b1a80586c074c54d93806027c28
Abdul Sayed <abdulrahim.s@gmail.com>
2026-05-14 21:00:19 +0530

    ldap /SSO login

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'tNJP-5c_rRCV8dxWEdLnx93K2dTnJN3S',
+            'X-App-Proxy': '9Pxg8kFHeAMVvJC2DNBhB8A_SiV-5sey',
           },
           body: JSON.stringify(requestDetails),
         };
